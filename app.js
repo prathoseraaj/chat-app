8587adc1fdda385b33fd157261dfa77de36f3304
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-29 00:04:32 +0530

    feat: delete function

--- app.js
+++ app.js
@@ -2,19 +2,29 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 4000
-const server = app.listen(PORT, ()=>console.log(`💬 Server on port ${PORT}`))
+const server = app.listen(PORT, () => console.log(`💬 Server on port ${PORT}`))
 
 const io = require('socket.io')(server)
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 //to create the set of socketid
 let connectedids = new Set()
 
 io.on('connection', onConnect)
 
-function onConnect(socket){
+function onConnect(socket) {
     console.log(socket.id) // socket id { AND if we open in another brower tab, it produce another serverid} 
     connectedids.add(socket.id)
     //console.log(connectedids); {To check}
+
+
+    io.emit('client-total', connectedids.size)
+
+    socket.on('disconnect', () => {
+        console.log('socket disconnected', socket.id)
+        connectedids.delete(socket.id);
+        io.emmit('client-total', connectedids.size)
+
+    })
 }
