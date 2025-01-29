23b30bc658002cbb059dd8aa476bcffffae55bdc
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-29 11:42:04 +0530

    feat: creating a json

--- public/main.js
+++ public/main.js
@@ -1,6 +1,21 @@
 const socket = io() 
 
+const messagecontainer = document.getElementById('message-container')
+const nameinput = document.getElementById('name-input')
+const messageform = document.getElementById('message-form')
+const messageinput = document.getElementById('message-input')
+
+messageform.addEventListener('submit',(e) =>{
+    e.preventDefault();
+    sendMessage();
+})
+
 socket.on('client-total',(data) =>{
     console.log(`Total client: ${data}`)
     document.getElementById('clients-total').innerHTML = `Total Clients: ${data}`
 })
+
+function sendMessage (){
+    console.log(messageinput.value);
+}
+
