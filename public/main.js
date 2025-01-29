a8f1c7094d83ecda27e6fc20a1ebf3ba8e11e095
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-29 19:04:59 +0530

    feat: send message

--- public/main.js
+++ public/main.js
@@ -1,30 +1,48 @@
-const socket = io() 
+const socket = io()
 
 const messagecontainer = document.getElementById('message-container')
 const nameinput = document.getElementById('name-input')
 const messageform = document.getElementById('message-form')
 const messageinput = document.getElementById('message-input')
 
-messageform.addEventListener('submit',(e) =>{
+messageform.addEventListener('submit', (e) => {
     e.preventDefault();
     sendMessage();
 })
 
-socket.on('client-total',(data) =>{
+socket.on('client-total', (data) => {
     console.log(`Total client: ${data}`)
     document.getElementById('clients-total').innerHTML = `Total Clients: ${data}`
 })
 
-function sendMessage (){
+function sendMessage() {
     console.log(messageinput.value);
     const data = {
         name: nameinput.value,
         message: messageinput.value,
-        datetime: new Date(),       
+        datetime: new Date(),
     }
-    socket.emit('message',data)
+    socket.emit('message', data)
+    addmessage(true,data)
+    messageinput.value = '';
 }
 
-socket.on('chat-message',(data)=>{
+socket.on('chat-message', (data) => {
     console.log(data)
+    addmessage(false,data)
 })
+
+function addmessage(isownmessage, data) {
+    const element = `
+    <li class=${isownmessage?"message-right":"message-left"}>
+        <p class="message">${data.message} </p>
+        <span>${data.name} ● ${moment(data.datetime).fromNow()}</span>
+    </li>`
+
+    messagecontainer.innerHTML += element
+    scrolltobutton()
+}
+
+function scrolltobutton(){
+    messagecontainer.scrollTo(0, messagecontainer.scrollHeight)
+}
